1168d229220d1b666e45d38b788b791645bf2166
Rashnil2016 <rashnil@live.unc.edu>
2016-11-28 00:40:34 -0500

    Loop through 840 hdfs directories and merge the files on 10 node high computing cluster

--- src/main/java/data/concatenate/JsonConcatDriver.java
+++ src/main/java/data/concatenate/JsonConcatDriver.java
@@ -1,16 +1,18 @@
 package data.concatenate;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.io.Text;
 
 import java.io.File;
+import java.net.URI;
 
 
 /**
@@ -22,6 +24,9 @@ public class JsonConcatDriver {
             throws Exception
     {
         Configuration conf = new Configuration();
+        //String uri ="hdfs://comet-21-67.ibnet:54310/";
+        String uri = conf.get("fs.default.name");
+
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length < 2)
         {
@@ -29,9 +34,15 @@ public class JsonConcatDriver {
             System.exit(2);
         }
 
-
         System.out.println("Input Path :"+otherArgs[0]);
         System.out.println("Output Path :"+otherArgs[1]);
+
+
+        String hdfs_path=uri + File.separator+otherArgs[0];
+
+        FileSystem fs= FileSystem.get(new URI(uri),conf);
+        FileStatus[] fileStatus = fs.listStatus(new Path(hdfs_path));
+
 
         Job job = new Job(conf, "Concatenate JSON");
         job.setJarByClass(JsonConcatDriver.class);
@@ -43,18 +54,38 @@ public class JsonConcatDriver {
         job.setOutputValueClass(NullWritable.class);
 
         //Read files from multiple subfolder
-        for (int i = 0; i < otherArgs.length - 1; i++) {
+      /*  for (int i = 0; i < otherArgs.length - 1; i++) {
+
+            System.out.println("Inside otherargs 1");
 
             File _file=new File(otherArgs[0]);
+            System.out.println("Inside otherargs 2");
             String [] dir=_file.list();
+            System.out.println("Inside otherargs 3");
 
-            for(int j=0;j<dir.length;j++)
-            {
-                FileInputFormat.addInputPath(job, new Path(otherArgs[i]+File.separator+dir[j]));
-            }
+            for(int p=0;p<dir.length;p++) {
+                System.out.println("directories :" + dir[p]);
+            } */
 
+        for(FileStatus _dfs : fileStatus)
+        {
 
+            System.out.println(_dfs.getPath().toString());
+
+            //FileInputFormat.addInputPath(job, new Path(otherArgs[i]+File.separator+dir[j]));
         }
+
+
+            for(FileStatus dfs : fileStatus)
+            {
+
+                FileInputFormat.addInputPath(job, new Path(dfs.getPath().toString()));
+
+                //FileInputFormat.addInputPath(job, new Path(otherArgs[i]+File.separator+dir[j]));
+           }
+
+
+        //}
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[(otherArgs.length - 1)]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
